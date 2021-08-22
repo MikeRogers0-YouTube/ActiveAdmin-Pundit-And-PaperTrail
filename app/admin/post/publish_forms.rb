4e2e71b5b97adcd1d1002abebec0d9de9d3941ed
Mike Rogers <me@mikerogers.io>
2021-08-22 18:40:36 -0500

    I think this form object approach in ActiveAdmin works

--- app/admin/post/publish_forms.rb
+++ app/admin/post/publish_forms.rb
@@ -1,5 +1,10 @@
 ActiveAdmin.register Post::PublishForm do
+  actions :index, :new, :create
 
+  belongs_to :post, class_name: "Post"
+
+  permit_params :terms_of_service
+  #
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
@@ -15,4 +20,24 @@ ActiveAdmin.register Post::PublishForm do
   #   permitted
   # end
   
+  form do |f|
+    f.inputs do
+      f.input :terms_of_service, as: :boolean
+    end
+    f.actions
+  end
+
+  controller do
+    def index
+      redirect_to([:admin, parent])
+    end
+
+    def build_new_resource
+      Post::PublishForm.new
+    end
+
+    def find_resource
+      Post::PublishForm.new
+    end
+  end
 end
--- app/forms/post/publish_form.rb
+++ app/forms/post/publish_form.rb
@@ -1,11 +1,16 @@
 class Post::PublishForm < ApplicationForm
+  attr_accessor :post_id
   attr_accessor :post
   attr_accessor :terms_of_service
 
-  validates :post, presence: true
+  validates :post_id, presence: true
   validates :terms_of_service, acceptance: true
 
   def attributes
     { 'terms_of_service' => terms_of_service }
+  end
+
+  def save
+    return unless valid?
   end
 end
